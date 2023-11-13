ad06be920223254414bfa9cbc2ed11e3d1a42960
Viktoryia Ivanova <good.qa.engineer@gmail.com>
2023-11-12 17:32:21 -0800

    fix plural selectors

--- linkedineasyapply.py
+++ linkedineasyapply.py
@@ -126,7 +126,7 @@ class LinkedinEasyApply:
             self.scroll_slow(job_results)
             self.scroll_slow(job_results, step=300, reverse=True)
 
-            job_list = self.browser.find_element(By.CLASS_NAME, 'jobs-search-results-list').find_elements(By.CLASS_NAME, 'jobs-search-results__list-item')
+            job_list = self.browser.find_elements(By.CLASS_NAME, 'jobs-search-results-list').find_elements(By.CLASS_NAME, 'jobs-search-results__list-item')
         except:
             raise Exception("No more jobs on this page")
 
@@ -249,7 +249,7 @@ class LinkedinEasyApply:
                 traceback.print_exc()
                 self.browser.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss').click()
                 time.sleep(random.uniform(3, 5))
-                self.browser.find_element(By.CLASS_NAME, 'artdeco-modal__confirm-dialog-btn')[1].click()
+                self.browser.find_elements(By.CLASS_NAME, 'artdeco-modal__confirm-dialog-btn')[1].click()
                 time.sleep(random.uniform(3, 5))
                 raise Exception("Failed to apply to job!")
 
@@ -274,7 +274,7 @@ class LinkedinEasyApply:
 
     def home_address(self, element):
         try:
-            groups = element.find_element(By.CLASS_NAME, 'jobs-easy-apply-form-section__grouping')
+            groups = element.find_elements(By.CLASS_NAME, 'jobs-easy-apply-form-section__grouping')
             if len(groups) > 0:
                 for group in groups:
                     lb = group.find_element(By.TAG_NAME, 'label').text.lower()
@@ -303,12 +303,12 @@ class LinkedinEasyApply:
 
     def additional_questions(self):
         #pdb.set_trace()
-        frm_el = self.browser.find_element(By.CLASS_NAME, 'jobs-easy-apply-form-section__grouping')
+        frm_el = self.browser.find_elements(By.CLASS_NAME, 'jobs-easy-apply-form-section__grouping')
         if len(frm_el) > 0:
             for el in frm_el:
                 # Radio check
                 try:
-                    radios = el.find_element(By.CLASS_NAME, 'jobs-easy-apply-form-element').find_element(By.CLASS_NAME, 'fb-text-selectable__option')
+                    radios = el.find_elements(By.CLASS_NAME, 'jobs-easy-apply-form-element').find_element(By.CLASS_NAME, 'fb-text-selectable__option')
 
                     radio_text = el.text.lower()
                     radio_options = [text.text.lower() for text in radios]
@@ -667,7 +667,7 @@ class LinkedinEasyApply:
 
     # Contact info fill-up
     def contact_info(self):
-        frm_el = self.browser.find_element(By.CLASS_NAME, 'jobs-easy-apply-form-section__grouping')
+        frm_el = self.browser.find_elements(By.CLASS_NAME, 'jobs-easy-apply-form-section__grouping')
         if len(frm_el) > 0:
             for el in frm_el:
                 text = el.text.lower()
@@ -688,7 +688,7 @@ class LinkedinEasyApply:
     def fill_up(self):
         try:
             easy_apply_content = self.browser.find_element(By.CLASS_NAME, 'jobs-easy-apply-content')
-            pb4 = easy_apply_content.find_elements(By.CLASS_NAME, 'jobs-easy-apply-form-element')
+            pb4 = easy_apply_content.find_elements(By.CLASS_NAME, 'pb4')
             if len(pb4) > 0:
                 for pb in pb4:
                     try:
@@ -721,13 +721,14 @@ class LinkedinEasyApply:
             writer.writerow(to_write)
 
     def scroll_slow(self, scrollable_element, start=0, end=3600, step=100, reverse=False):
-        if reverse:
-            start, end = end, start
-            step = -step
+        pass
+        # if reverse:
+        #     start, end = end, start
+        #     step = -step
 
-        for i in range(start, end, step):
-            self.browser.execute_script("arguments[0].scrollTo(0, {})".format(i), scrollable_element)
-            time.sleep(random.uniform(1.0, 2.6))
+        # for i in range(start, end, step):
+        #     self.browser.execute_script("arguments[0].scrollTo(0, {})".format(i), scrollable_element)
+        #     time.sleep(random.uniform(1.0, 2.6))
 
     def avoid_lock(self):
         if self.disable_lock:
